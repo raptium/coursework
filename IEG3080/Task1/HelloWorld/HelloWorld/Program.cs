d8ce145aa871880271fd76765a251e840207eba4
raptium <raptium@gmail.com>
2008-01-08 08:11:40 +0000

    IEG3080:Assignment 1:Task1 finished

---
+++ IEG3080/Task1/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Windows.Forms;
+
+namespace HelloWorld
+{
+    static class Program
+    {
+        static void Main()
+        {
+            MessageBox.Show("Hello c# world");
+        }
+    }
+}
