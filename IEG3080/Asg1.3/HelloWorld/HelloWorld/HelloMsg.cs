d9a05e91170520de147da5d94597b437580297ee
raptium <raptium@gmail.com>
2008-01-26 14:33:37 +0000

    IEG3080:Ass1 finished

--- IEG3080/Asg1.3/HelloWorld/HelloWorld/HelloMsg.cs
+++ IEG3080/Asg1.3/HelloWorld/HelloWorld/HelloMsg.cs
@@ -2,11 +2,11 @@ using System;
 using System.Windows.Forms;
 
 #if STRONG
-[assembly: System.Reflection.AssemblyVersion("1.0.0.0")]
+[assembly: System.Reflection.AssemblyVersion("1.0.0.1")]
 [assembly: System.Reflection.AssemblyKeyFile("myKey.snk")]
 #endif
 public class HelloMsg{
     public void Write(){
-        MessageBox.Show("Hello c# world");
+        MessageBox.Show("Hello new world");
     }
 }
