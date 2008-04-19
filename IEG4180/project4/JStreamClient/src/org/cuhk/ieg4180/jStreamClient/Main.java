fbc5dbbb15157f107a7f5718c518c5a070690b04
raptium <raptium@gmail.com>
2008-04-19 12:11:15 +0000

    IEG4180:project4 ...

--- IEG4180/project4/JStreamClient/src/org/cuhk/ieg4180/jStreamClient/Main.java
+++ IEG4180/project4/JStreamClient/src/org/cuhk/ieg4180/jStreamClient/Main.java
@@ -44,7 +44,7 @@ class mediaServer extends Thread{
     
     private SocketChannel m_channel;
     private HttpServer m_server;
-    private String media_dir = "c:\\temp\\media\\";
+    private String m_cacheDir = "c:\\temp\\media\\cache\\";
     private Hashtable<String, String> m_mime;
     private String m_filename;
     private int m_port;
@@ -80,8 +80,39 @@ class mediaServer extends Thread{
         }
         else
             throw new Exception();
-        
     }
+    
+    
+    private void Error404(Exception ex){
+        HttpResponse response = new HttpResponse();
+        
+        try{
+            response.setStatus("404");
+            response.setHeader("Connection", "close");
+            response.setHeader("Content-Type", "text/html;charset=iso-8859-1");
+            response.fillMessage(ex.getMessage().getBytes());
+            m_server.sendResponse(m_channel, response);
+            m_channel.close();
+        }catch(Exception e){
+            
+        }
+    }
+    
+    private void send200(){
+        HttpResponse response = new HttpResponse();
+        
+        try{
+            response.setStatus("200");
+            response.setHeader("Connection", "close");
+            response.setHeader("Content-Type", "text/html;charset=iso-8859-1");
+            response.fillMessage("".getBytes());
+            m_server.sendResponse(m_channel, response);
+            m_channel.close();
+        }catch(Exception e){
+            
+        }
+    }
+    
     
     @Override
     public void run(){
@@ -90,95 +121,115 @@ class mediaServer extends Thread{
         FileInputStream iStream = null;
         streamClient sClient = null;
         
+        
+        // decode the URL escape character if possible
         try {
-            path = URLDecoder.decode(request.getPath(), "utf-8"); //decode the URL escape character
+            path = URLDecoder.decode(request.getPath(), "utf-8"); 
         } catch(UnsupportedEncodingException ex) {
             path = request.getPath();
         }
+        
+        
+        //  parse the parameters from the PATH field of a HTTP Request
         try {
             parsePath(path);
         } catch(Exception ex){
-            System.out.println("Invalid request.");
-            return;
+            System.out.println("Invalid request."); // Cannot continue if the request is invalid
+            Error404(ex);
+            return; // Error 404 here
         }
         
         
         try{
-            iStream = new FileInputStream(new File(media_dir + m_filename));
+            // Try to find the file in cache directory first
+            iStream = new FileInputStream(new File(m_cacheDir + m_filename));
         }catch(Exception ex){
-            System.out.println("Canot find the requested file in cache.");
-            System.out.println("Try to get the file from streamming server.");
+            System.out.println("File not found in cache.");
+            // It seems we cannot find the file in the cache directory
+            // Therefore send the request to the stream server
             
             try {
+                // Try to connect to the stream server
                 sClient = new streamClient(m_host, m_port, m_packetSize, m_protocol, m_filename);
                 sClient.connect();
             } catch (Exception ex1){
-                System.out.println("Server connect failed.");
-                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex1);
+                // Cannot connect to the stream server
                 
-                try{
-                    HttpResponse response = new HttpResponse();
-                    response.setStatus("404"); //HTTP 404
-                    response.fillMessage("Cannot find the request file.".getBytes());
-                    response.setHeader("Connection", "close");
-                    m_server.sendResponse(m_channel, response);
-                    m_channel.close();
-                }catch(Exception ex2){
-                    
-                    
-                }
-                    
-                return;
+                System.out.println("Server connect failed.");
+                Error404(ex1);
+                return; // Error 404 here
             }
         }
         
-        System.out.print(request.toString());
-        byte[] buffer = new byte[1024];
+        
+        /*
+        if(!request.toString().contains("Pragma")){
+            send200();
+            return;
+        }
+        */
+        
+        System.out.print(request.toString()); // for debug only
 
+        
+        int len = 0;
         try{
+            if(iStream != null){
+                len = iStream.available();
+            }else{
+                len = sClient.getLength();
+            }
+        }catch(Exception ex){
+            
+        }
+
+        byte[] buffer = new byte[m_packetSize];
+        try{
+            System.out.println("here.");
             
             HttpResponse response = new HttpResponse();
-            response.setStatus("200"); //HTTP 200 OK
-            //Set the default MIME type
+            response.setStatus("200"); // HTTP 200 OK
+            // Set the default MIME type
             response.setHeader("Content-Type", "application/octet-stream"); 
             
             
             Pattern pattern = Pattern.compile("\\.(.*)$"); 
             Matcher matcher = pattern.matcher(m_filename);
-            boolean found = matcher.find(); //Get the file extension
+            boolean found = matcher.find(); // Get the file extension
             if(found){
                 String ext = matcher.group(1);
-                if(m_mime.get(ext) != null) //Lookup the hashtable
-                    response.setHeader("Content-Type", m_mime.get(ext)); //Set the correct MIME type
+                if(m_mime.get(ext) != null) // Lookup the hashtable
+                    response.setHeader("Content-Type", m_mime.get(ext)); // Set the correct MIME type
             }
 
             response.setHeader("Connection", "close");
-            response.fillMessage(new String("").getBytes()); //Add empty data message
-            //Set the Content-Length to the whole length of the file
-            //response.setHeader("Content-Length", "" + iStream.available());
-            m_server.sendResponse(m_channel, response); //Send the HTTP response with header only
+            response.fillMessage(new String("").getBytes()); // Add empty data message
+            // Set the Content-Length to the whole length of the file
+            response.setHeader("Content-Length", "" + len);
+            m_server.sendResponse(m_channel, response); // Send the HTTP response with header only
             
-            //For transmission rate control
+            // For transmission rate control
             long startTime = System.currentTimeMillis();
             int sent = 0;
             long sleepTime;
-            double speed = 1024 * 256; //256KB/s should be enough for streaming
+            double speed = 1024 * 256; // 256KB/s should be enough for streaming
             int packetSize = buffer.length;
-
+            int received = 0;
+            
             while(true) {
                 int length;
-                if(iStream != null)
+                if(iStream != null) // If we are reading from the cache file
                     length = iStream.read(buffer);
                 else{
+                    // If we are streaming form the server
                     length = sClient.read(buffer);
-                    if(sent == 0){
-                        sent++;
-                        continue;
-                    }
                 }
                 if(length <= 0)
                     break;
+                received += length;
                 m_server.sendData(m_channel, buffer, length); //Send the streaming data
+                if(received >= len)
+                    break;
                 
                 //rate control
                 sent ++;
@@ -189,6 +240,8 @@ class mediaServer extends Thread{
             }
             if(iStream != null)
                 iStream.close();
+            else
+                sClient.finish();
             m_channel.close();
         }catch(Exception ex){
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
--- IEG4180/project4/JStreamClient/src/org/cuhk/ieg4180/jStreamClient/libStream/streamClient.java
+++ IEG4180/project4/JStreamClient/src/org/cuhk/ieg4180/jStreamClient/libStream/streamClient.java
@@ -5,10 +5,12 @@
 
 package org.cuhk.ieg4180.jStreamClient.libStream;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.*;
 import java.nio.*;
 import java.nio.channels.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -20,8 +22,9 @@ public class streamClient {
     private InetSocketAddress m_remote;
     private String m_filename;
     private int m_packetSize;
-    
     private int m_protocol;
+    private mediaCache m_cache;
+    private int m_contentLength;
     
     
     public streamClient(String host, int port, int packetSize,String protocol, String filename) throws java.io.IOException{
@@ -46,16 +49,16 @@ public class streamClient {
     
     public final static int swabInt(int v) {
         return  (v >> 24) | (v << 24) |
-                ((v << 8) & 0x00FF0000) | ((v >> 8) & 0x0000FF00);
+                ((v << 8) & 0x00ff0000) | ((v >> 8) & 0x0000ff00);
     }
     
     public void connect() throws java.io.IOException{
         ByteBuffer buffer = ByteBuffer.allocate(256);
-        
+        int ret;
         
         buffer.putInt(0, swabInt(m_packetSize));
-        buffer.putInt(12, swabInt(10101));
-        buffer.position(16);
+        buffer.putInt(8, swabInt(-1));
+        buffer.position(12);
         buffer.put(m_filename.getBytes());
         buffer.rewind();
         
@@ -70,6 +73,26 @@ public class streamClient {
         else{
             m_udpChannel.write(buffer);
         }
+        
+        ByteBuffer bBuffer = ByteBuffer.allocate(m_packetSize);
+        try{
+            if(m_protocol == 1){
+                ret = m_tcpChannel.read(bBuffer);
+            }
+            else{
+                ret = m_udpChannel.read(bBuffer);
+            }
+            m_contentLength = bBuffer.getInt(0);
+        }catch(java.io.IOException ex){
+            return;
+        }
+        
+        // create cache after connected
+        m_cache = new mediaCache(m_filename);
+    }
+    
+    public int getLength(){
+        return m_contentLength;
     }
     
     
@@ -89,7 +112,50 @@ public class streamClient {
         }
         
         System.arraycopy(bBuffer.array(), 0, buffer, 0, buffer.length);
-       
+        
+        try{
+            m_cache.write(buffer);
+        } catch (IOException ex) {
+            Logger.getLogger(streamClient.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
         return length;
+    }
+    
+    public void finish(){
+        try {
+            m_cache.save();
+        } catch (IOException ex) {
+            Logger.getLogger(streamClient.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
+
+
+class mediaCache{
+    private final String m_cacheDir = "c:\\temp\\media\\cache\\";
+    private String m_filename;
+    private FileOutputStream m_stream;
+    private File m_tempFile;
+    
+    
+    public mediaCache(String filename) throws FileNotFoundException, SecurityException{
+        m_filename = filename;
+        m_tempFile = new File(m_cacheDir + m_filename + ".tmp");
+        m_stream = new FileOutputStream(m_tempFile);
+    }
+    
+    public void write(byte[] buffer) throws IOException{
+        m_stream.write(buffer);
+    }
+    
+    public void save() throws IOException{
+        m_stream.close();
+        try{
+            m_tempFile.renameTo(new File(m_cacheDir + m_filename));
+        }
+        catch (Exception ex) {
+            Logger.getLogger(streamClient.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
